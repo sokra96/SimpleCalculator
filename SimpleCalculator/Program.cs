5a742d20bed0d523b30070e898fef73fde3e552c
Sokratis Mantzakos <mantzakossokratis@gmail.com>
2026-01-23 16:09:35 +0200

    Stage 1 – Input validation and basic + / - operations

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,13 +1,45 @@
-﻿Console.WriteLine("Welcome to Simple Calculator!");
+﻿using System.Globalization;
+
+Console.WriteLine("Welcome to Simple Calculator!");
 
 Console.WriteLine("Εnter the first number: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1;
+while (!int.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine("Invalid input! Please enter a number");
+}
 
 Console.WriteLine("Enter the second number: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2;
+while (!int.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine("Invalid input! Please enter a number");
+}
 
-int result = num1  + num2;
 
+int result;
+while (true)
+{
+    Console.WriteLine("Enter operation (+ or -): ");
+    String operation = Console.ReadLine();
+    if (operation == "+")
+    {
+        result = num1 + num2;
+        break;
+    }
+    else if (operation == "-")
+    {
+        result = num1 - num2;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid operation (+ or -)");
+    }
+   
+}
 Console.WriteLine("Result is : " + result);
 Console.ReadKey();
+
+
 
