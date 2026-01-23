84e210697a12d962b6b06dcb8511a2be12ad29d9
Sokratis Mantzakos <mantzakossokratis@gmail.com>
2026-01-23 15:25:11 +0200

    Stage 0 – Initial calculator: add two numbers

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Welcome to Simple Calculator!");
+
+Console.WriteLine("Εnter the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int result = num1  + num2;
+
+Console.WriteLine("Result is : " + result);
+Console.ReadKey();
+
